664437cecc21b2dea6963e57fd53a7047e5ed8a1
arhaval <arhaval2024@gmail.com>
2025-12-19 02:07:46 +0300

    fix: Image import kaldırıldı - login-selection sayfası

--- app/login-selection/page.tsx
+++ app/login-selection/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import Link from 'next/link'
-import Image from 'next/image'
 import { Video, Mic, UserCheck, Shield } from 'lucide-react'
 
 export default function LoginSelectionPage() {
