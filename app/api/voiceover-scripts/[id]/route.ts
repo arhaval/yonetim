bc142927a69983a4439062583275f2d9cd3804fc
arhaval <arhaval2024@gmail.com>
2025-12-28 19:37:48 +0300

    Add missing imports to route.ts

--- app/api/voiceover-scripts/[id]/route.ts
+++ app/api/voiceover-scripts/[id]/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { prisma } from '@/lib/prisma'
 import { cookies } from 'next/headers'
-import { canViewVoiceover } from '@/lib/voiceover-permissions'
+import { canViewVoiceover, canEditVoiceLink, canDeleteVoiceover, isAdmin } from '@/lib/voiceover-permissions'
 
 // Metin detayını getir
 export async function GET(
